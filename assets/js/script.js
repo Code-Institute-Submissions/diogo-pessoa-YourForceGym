a4b749ac030ec065d18cf02696fc52a632c9acc3
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-01-12 21:57:18 +0000

    Gallery finished
    - video aligned with heading, using bootstrap columns for better positioning
    - gallery adjusted using bootstrap columns grid system
    - added new images
    - modal working with custom js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
-$("#image-thumb-expand").on("click", function() {
-    $('#imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
-    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
- });
+$(function() {
+    $('.image-thumb-expand').on('click', function() {
+        console.log($(this).attr('src'))
+        $('.imagepreview').attr('src', $(this).attr('src'));
+        $('#imagemodal').modal('show');   
+    });		
+});
