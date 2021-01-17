2c6ad8335a75bec2694b4684f592666a5750f94d
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-01-17 17:59:25 +0000

    Refactoring document formmating

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@ $(function() {
     $('.image-thumb-expand').on('click', function() {
         console.log($(this).attr('src'))
         $('.imagepreview').attr('src', $(this).attr('src'));
-        $('#imagemodal').modal('show');   
-    });		
+        $('#image-gallery-modal').modal('show');
+    });
 });
